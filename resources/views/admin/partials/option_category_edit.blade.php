c4439ed3b0e4813eaf99384247fa46799799c41d
ngovannhan2004 <buivannhan20032004@gmail.com>
2023-05-25 02:10:32 +0700

    update

---
+++ resources/views/admin/partials/option_category_edit.blade.php
@@ -0,0 +1,7 @@
+
+<option @if($childCategory->id == $editCategory->parent_id) selected @endif value="{{ $childCategory->id }}">{{ $name_extra.' '.$childCategory->name }}</option>
+@if ($childCategory->childrens->count() > 0)
+    @foreach ($childCategory->childrens as $grandChildCategory)
+        @include('admin.partials.option_category', ['childCategory' => $grandChildCategory, 'name_extra' => $name_extra.'--'])
+    @endforeach
+@endif
